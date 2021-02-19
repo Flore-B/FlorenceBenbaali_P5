a479c919100bd3d363e77f7d7191d547120cd87d
florence_benbaali <florence.benbaali@cegetel.net>
2021-02-19 13:17:48 +0100

    Ajout commentaires

--- js/ajax.js
+++ js/ajax.js
@@ -14,26 +14,27 @@ const card = urlParams.get("id");
 async function descriptiveProduct() {
   await fetch("http://localhost:3000/api/teddies/" + card)
     .then(response => response.json())
-    .then(data => oneProduct(data)) //appelle fonction pour la page produit
+    .then(data => oneProduct(data)) //appelle fonction pour création page produit
     .catch(error => alert("L'appel au serveur ne fonctionne pas : " + error)); //si la promesse est rejetée
 }
 
-//-----------------Fonction pour envoyer les données récoltées à l'API----------------------
+//-------------Fonction pour envoyer les données récoltées à l'API---------------
 
 function validOrder() {
 
   let button = document.querySelector(".submit");
   button.addEventListener("click", () => {
-   
-    let prenom = document.getElementById("prenom").value;
+
+ //------------------Récupération des données du formulaire----------------------
+      let prenom = document.getElementById("prenom").value;
     let nom = document.getElementById("nom").value;
     let adresse = document.getElementById("adresse").value;
     let ville = document.getElementById("ville").value;
     let couriel = document.getElementById("email").value;
 
-    let products = totalBasket.map((totalBasket) => totalBasket.id);
+    let products = totalBasket.map((totalBasket) => totalBasket.id);//création d'un tableau avec seulement les id des produits
   
-    let order = {
+    let order = { //création de l'objet contenant les données du contact et les id des produits sélectionnés
       contact: {
         firstName: prenom,
         lastName: nom,
@@ -43,20 +44,21 @@ function validOrder() {
       },
       products: products,
     };
+  //--------------------Envoi des données au serveur---------------------------
      fetch("http://localhost:3000/api/teddies/order",
       {
         method: "POST",
-        body: JSON.stringify(order),
+        body: JSON.stringify(order), //envoi de l'objet au format JSON
         headers: {
           "Content-Type": "application/json",
         },
       })
         .then(response => response.json())
         .then(response => {
-          localStorage.setItem('order', JSON.stringify(order));
-          localStorage.setItem('firstName', JSON.stringify(response.contact.firstName));
-          localStorage.setItem('orderId', JSON.stringify(response.orderId));
-          window.location.href = "/front/confirmation.html";
+          localStorage.setItem('order', JSON.stringify(order)); //enregistrement de l'objet dans le local storage
+          localStorage.setItem('firstName', JSON.stringify(response.contact.firstName)); //stockage de l'objet dans le local storage
+          localStorage.setItem('orderId', JSON.stringify(response.orderId)); //stockage du numéro de commande
+          window.location.href = "/front/confirmation.html"; //ouverture de la page confirmation
         })
     });
   }
